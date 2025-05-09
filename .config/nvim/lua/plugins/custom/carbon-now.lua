fb1ca9efa7fa369f62180765e289d89c8f90d2ce
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-05-09 14:47:01 +0300

    delete carbon-now keybinding

--- .config/nvim/lua/plugins/custom/carbon-now.lua
+++ .config/nvim/lua/plugins/custom/carbon-now.lua
@@ -1,6 +1,5 @@
 return {
   "ellisonleao/carbon-now.nvim",
-  keys = { { "<A-Space>n", vim.cmd.CarbonNow, mode = "v", desc = "Carbon Now" } },
   cmd = "CarbonNow",
   opts = {
     base_url = "https://carbon.now.sh/",
