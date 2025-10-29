4828d97ab159684cc193eb12b5e450370ec975dd
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-10-29 10:34:34 +0300

    add top buttons to alpha-nvim

--- .config/nvim/lua/plugins/alpha-nvim.lua
+++ .config/nvim/lua/plugins/alpha-nvim.lua
@@ -16,6 +16,11 @@ return {
 
     ]]
     startify.section.header.val = vim.split(logo, "\n")
+    startify.section.top_buttons.val = {
+      startify.button("e", "  New file", ":ene <BAR> startinsert<CR>"),
+      startify.button("d", "  Restore current dir session", "<cmd>lua require('persistence').load()<CR>"),
+      startify.button("r", "󰑓  Restore last session", "<cmd>lua require('persistence').load({ last = true })<CR>"),
+    }
     require("alpha").setup(require("alpha.themes.startify").config)
     vim.keymap.set("n", "<A-Space>a", ":Alpha<cr>", { desc = "Alpha Toggle", silent = true })
   end,
