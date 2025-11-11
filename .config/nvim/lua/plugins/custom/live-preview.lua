608d1c006f2d773134139700d406b5244a35850d
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-11-11 17:59:09 +0200

    add markdown-preview nvim plugin + remove live-preview.lua & peek.lua + update nvim plugins

--- .config/nvim/lua/plugins/custom/live-preview.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "brianhuster/live-preview.nvim",
-}
---
+++ .config/nvim/lua/plugins/custom/markdown-preview.lua
@@ -0,0 +1,12 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+  config = function()
+    vim.keymap.set("n", "<A-Space>m", ":MarkdownPreviewToggle<cr>", { desc = "Markdown Preview", silent = true })
+  end
+}
